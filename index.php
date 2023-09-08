f83cc71b61e6bd5f3c77cfe117ceb9adeb73ccbd
M. Agus Badruzaman Al Khoir <hoir65@gmail.com>
2023-09-08 17:19:20 +0700

    Update Tampilan

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Crop and Download Image</title>
+    <title>Penerjemah Ballon Manga</title>
     <!-- Google Fonts -->
     <link
       href="https://fonts.googleapis.com/css2?family=Rubik&display=swap"
@@ -15,59 +15,116 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.13/cropper.min.css"
     />
     <!-- Stylesheet -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
+
+      <div class="navku" id="navbar">
+        <nav class="navbar navbar-expand-lg navbar-dark ">
+          <a class="navbar-brand" href="#" style="color:white;"><b>Penerjemah Ballon Manga</b></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="https://ruangilmudigital.com">Situs <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Penerjemah <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Dokumentasi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.youtube.com/@ruangilmudigital7593">Youtube</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
     <div class="wrapper">
-      <div class="container">
+
+      <div class="row">
+      <div class="container col-10">
+        
+        <div class="rounded border" style="min-height: 600px;">
+          <div id="hint" class="text-info"  align="center"><br>Silahkan<br>Upload Gambar</div>
         <div class="image-container">
           <img id="image" />
         </div>
+        </div>
+        
         <div class="preview-container">
+        <div class="rounded border" align="center" style="min-height: 300px;">
+          <div id="hint2" class="text-info"  align="center"><br>Belum ada<br>Proses</div>
           <img id="preview-image" />        
           <div id="result-container"></div> <!-- Wadah untuk menampilkan hasil dari server -->
+        </div>      
+        </div>
+
+      </div>
+      <div class="col-2">
+        
+        <input type="file" id="file" accept="image/*" />
+      <label for="file">Upload Gambar</label><hr>
+      <div class="options hide">
+        Ukuran Selector
+        <div class="row" align="center">
+          <input class="col" 
+            type="number"
+            id="height-input"
+            placeholder="Enter Height"
+            max="780"
+          />
+          <input class="col" 
+            type="number"
+            id="width-input"
+            placeholder="Enter Width"
+            max="780"
+          />
+        </div><hr>  
+        Skala Selector<br>
+        <div align="">
+        <button style="min-width: 70px;" class="aspect-ratio-button">16:9</button>
+        <button style="min-width: 70px;" class="aspect-ratio-button">4:3</button>
+        <button style="min-width: 70px;" class="aspect-ratio-button">1:1</button>
+        <button style="min-width: 70px;" class="aspect-ratio-button">2:3</button>
+        <button style="min-width: 70px;" class="aspect-ratio-button">Free</button>
         </div>
       </div>
-      <input type="file" id="file" accept="image/*" />
-      <label for="file">Upload Gambar</label>
-      <div class="options hide">
-        <input
-          type="number"
-          id="height-input"
-          placeholder="Enter Height"
-          max="780"
-        />
-        <input
-          type="number"
-          id="width-input"
-          placeholder="Enter Width"
-          max="780"
-        />
-        <button class="aspect-ratio-button">16:9</button>
-        <button class="aspect-ratio-button">4:3</button>
-        <button class="aspect-ratio-button">1:1</button>
-        <button class="aspect-ratio-button">2:3</button>
-        <button class="aspect-ratio-button">Free</button>
-      </div>
+      <hr>
       <div class="btns">
-        <button id="preview" class="hide">Ambil</button>
-        <a href="" id="download" class="hide">Download</a>
+
+        <button style="padding: 12.5px; margin-bottom: 20px;" id="preview" class="hide">Ambil Gambar</button>
+        <a href=""  id="download" class="hide">Download</a>
       </div>
-      <div class="btns" align="center">
+      <hr>
+      <div class="btns" >
         <form id="my-form">
-         <select style="border-radius: 5px; padding: 8px; margin-bottom: 10px;" name="bahasa">
+         <select id='bhs' style="border-radius: 5px; padding: 8px; margin-bottom: 10px;" name="bahasa">
             <option selected value="id">Bahasa Indonesia</option>
             <option value="en">Bahasa Inggris</option>
             <!-- Tambahkan opsi bahasa lain sesuai kebutuhan -->
         </select>
-          <button type="submit" id="tls">Terjemahkan</button>
+          <button type="submit" id="tls">Terjemahkan Kalimat</button>
         <form id="my-form" enctype="multipart/form-data">
       </div>
+
+      </div>
+      </div>
+      
+      
     </div>
+    <div style="color: white; margin-top: 830px; color: #ccc;" align="center"> Dibuat oleh Al Khoir, Di dukung @ruangilmuditial.com</div>
     <!-- Script -->
     <script type="text/javascript">
 
 document.getElementById('tls').style.display = 'none';
+document.getElementById('bhs').style.display = 'none';
+document.getElementById('hint').style.display = 'block';
 let fileInput = document.getElementById("file");
 let image = document.getElementById("image");
 let downloadButton = document.getElementById("download");
@@ -86,6 +143,8 @@ fileInput.onchange = () => {
   widthInput.value = 0;
   downloadButton.classList.add("hide");
   document.getElementById('tls').style.display = 'none';
+document.getElementById('bhs').style.display = 'none';
+document.getElementById('hint').style.display = 'none';
 
   //The FileReader object helps to read contents of file stored on the computer
   let reader = new FileReader();
@@ -137,6 +196,9 @@ previewButton.addEventListener("click", (e) => {
   e.preventDefault();
   downloadButton.classList.remove("hide");
   document.getElementById('tls').style.display = 'block';
+  document.getElementById('bhs').style.display = 'block';
+document.getElementById('hint').style.display = 'none';
+document.getElementById('hint2').style.display = 'none';
   let imgSrc = cropper.getCroppedCanvas({}).toDataURL();
 
    // Buat Blob dari Data URL
@@ -170,6 +232,8 @@ window.onload = () => {
   options.classList.add("hide");
   previewButton.classList.add("hide");
   document.getElementById('tls').style.display = 'none';
+document.getElementById('bhs').style.display = 'none';
+document.getElementById('hint').style.display = 'block';
 };
 
 document.getElementById('my-form').addEventListener('submit', function(event) {
@@ -199,5 +263,8 @@ function sendData() {
 }
 
     </script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   </body>
 </html>
